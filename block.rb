2d3b06aa0a6684c90df2d18c8a53bbbddba5a040
Pablo Valenzuela <pabloce.valenzuela@gmail.com>
2023-05-16 21:06:22 -0400

    added new component

---
+++ block.rb
@@ -0,0 +1,22 @@
+def multiple_of_five  
+    yield 1
+    yield 2
+    yield 3
+    yield 4
+    yield 5
+end
+
+multiple_of_five { |num| puts num * 5 }
+
+=begin 
+def explicit_block(&block) 
+    block.call
+end
+
+explicit_block { puts "Explicit block called"}
+=end
+
+
+
+
+
